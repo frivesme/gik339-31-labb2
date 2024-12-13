5055cb014b05b625e15d6a446cd2f773e5b1f34d
frivesme <sannaboholm@hotmail.com>
2024-12-13 12:19:14 +0100

    Version 4

--- client/script.js
+++ client/script.js
@@ -5,4 +5,20 @@ fetch(url)
     console.log(response);
     return response.json();
   })
-  .then((jsonData) => console.log(jsonData));
+  .then((jsonData) => {
+    //Uppgift 7
+    const newUl = document.createElement("ul");
+
+    //newUl.id = "newUl";
+
+    jsonData.forEach((user) => {
+      const html = `
+      <ul id = "newUl" style= "background-color: ${user.color}">
+        <p>${user.firstName} ${user.lastName} ${user.username} </p>
+       
+      </ul>`;
+
+      document.body.insertAdjacentHTML("beforeend", html);
+      console.log(jsonData);
+    });
+  });
